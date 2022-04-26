fa7322f0ee369758fa31a3ed91d063d58282c3cf
Nishad Saraf <nishad.saraf@xilinx.com>
2022-04-26 10:14:19 -0700

    Update license files
    Update license files.
    
    Signed-off-by: Nishad Saraf <nishad.saraf@xilinx.com>

--- settings.sh
+++ settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###############################################################################
-# Copyright (C) 2019 - 2021 Xilinx, Inc.  All rights reserved.
+# Copyright (C) 2019 - 2022 Xilinx, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 ###############################################################################
 
