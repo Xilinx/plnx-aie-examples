1db4336a2f66f4af77c0fabb57d7422941e61f9f
Hua Jiang <hua.jiang@xilinx.com>
2023-06-20 18:40:43 -0700

    add home and tmp folder checking

--- script/home_tmp_set.sh
+++ script/home_tmp_set.sh
@@ -4,8 +4,25 @@ directory="/scratch/staff/$USER"
 if [ ! -d "$directory" ]; then
     # Directory does not exist, create it
     mkdir -p "$directory"
-    echo "Created directory: $directory"
+		if [ $? -ne 0 ]; then
+				echo "$directory creation failed!"
+		else
+				echo "Created directory: $directory"
+		fi
 fi
+
+check_directory() {
+    if [ -d "$1" ]; then
+        echo "$1 exist"
+				directory=$1	
+    else
+        echo "Directory $1 not exist"
+        read -p "Please input another big space directory for HOME&TMP:" new_directory
+        check_directory "$new_directory"
+    fi
+}
+
+check_directory "$directory"
 
 # Update TMP, TEMP, and HOME environment variables
 export TMP="$directory"
