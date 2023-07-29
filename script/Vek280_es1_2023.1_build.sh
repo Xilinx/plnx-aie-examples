bc594c15fcbc1f88606522fcd497305f9f13b84c
Hua Jiang <hua.jiang@xilinx.com>
2023-07-28 21:54:45 -0700

    fix <iostream> not found issue
    PTLNX_BSP which reponse to get the header file etc will not work
    once SYSROOT get defined, in this example we using PTLNX_BSP to
    download BSP and avoid SYSROOT not exist issue, then need to unset
    the SYSROOT env to avoid such issue.

--- script/Vek280_es1_2023.1_build.sh
+++ script/Vek280_es1_2023.1_build.sh
@@ -13,8 +13,9 @@ source /proj/petalinux/2023.1/petalinux-v2023.1_daily_latest/tool/petalinux-v202
 
 export BASE_XSA=/proj/xbuilds/2023.1_daily_latest/internal_platforms/xilinx_vek280_es1_base_202310_1/hw/hw.xsa
 
+# clear existing SYSROOT as once SYSROOT defined PTLNX_BSP will not work
+unset SYSROOT
 #export SYSROOT=/proj/xbuilds/2023.1_daily_latest/internal_platforms/sw/versal/xilinx-versal-common-v2023.1/sysroots/cortexa72-cortexa53-xilinx-linux/
-
 #There is a petalinux minal build bug which add vdu patcket for all board that may have on vdo in part
 #This issue happen after CR-1152465, and 0131 and before build is still work
 export PTLNX_BSP=/proj/petalinux/2023.1/petalinux-v2023.1_01311535/bsp/release/xilinx-vek280-es1-v2023.1-final.bsp
@@ -27,4 +28,4 @@ cd ./designs/xgemm-gmio
 LINUX="linux"
 make AIEARCH=aie-ml OS=$LINUX
 
-make AIEARCH=aie-ml OS=baremetal
+#make AIEARCH=aie-ml OS=baremetal
