0a671153ce0904e30767a5df17d69650c7523be7
hua jiang <hua.jiang@amd.com>
2024-02-21 17:20:53 -0800

    add Vck190 compile script

--- script/Vck190_2023.1_build.sh
+++ script/Vck190_2023.1_build.sh
@@ -3,22 +3,21 @@
 #Fix build fail cause by HOME or tmp disk not enough issue
 source ./home_tmp_set.sh
 #Fix vek280 part not found in 2023.1 issue
-source ./enable_beta_part.sh
+#source ./enable_beta_part.sh
 
 export XILINXD_LICENSE_FILE=2100@aiengine-eng
 
-source /proj/xbuilds/2023.1_daily_latest/installs/lin64/Vitis/2023.1/settings64.sh
+source /proj/xbuilds/2023.2_daily_latest/installs/lin64/Vitis/2023.2/settings64.sh
 
-source /proj/petalinux/2023.1/petalinux-v2023.1_daily_latest/tool/petalinux-v2023.1-final/settings.sh
+source /proj/petalinux/2023.2/petalinux-v2023.2_daily_latest/tool/petalinux-v2023.2-final/settings.sh
 
-export BASE_XSA=/proj/xbuilds/2023.1_daily_latest/internal_platforms/xilinx_vek280_es1_base_202310_1/hw/hw.xsa
+export BASE_XSA=/proj/xbuilds/2023.2_daily_latest/internal_platforms/xilinx_vck190_base_202320_1/hw/hw.xsa
 
-# clear existing SYSROOT as once SYSROOT defined PTLNX_BSP will not work
-unset SYSROOT
 #export SYSROOT=/proj/xbuilds/2023.1_daily_latest/internal_platforms/sw/versal/xilinx-versal-common-v2023.1/sysroots/cortexa72-cortexa53-xilinx-linux/
+
 #There is a petalinux minal build bug which add vdu patcket for all board that may have on vdo in part
 #This issue happen after CR-1152465, and 0131 and before build is still work
-export PTLNX_BSP=/proj/petalinux/2023.1/petalinux-v2023.1_01311535/bsp/release/xilinx-vek280-es1-v2023.1-final.bsp
+export PTLNX_BSP=/proj/petalinux/2023.2/petalinux-v2023.2_stable_latest/bsp/release/xilinx-vck190-v2023.2-final.bsp
 
 cd ../
 
@@ -26,7 +25,6 @@ source ./settings.sh
 
 cd ./designs/xgemm-gmio
 LINUX="linux"
+#make AIEARCH=aie OS=$LINUX
 make clean
-make AIEARCH=aie-ml OS=$LINUX
-
-#make AIEARCH=aie-ml OS=baremetal
+make AIEARCH=aie OS=baremetal
